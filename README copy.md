dfd64218c49c3473fabc9c2ef4f9c683be35d7da
Tresz Adrián <152717454+Adrian1128@users.noreply.github.com>
2025-11-02 21:54:40 +0100

    add base repository structure

---
+++ README copy.md
@@ -0,0 +1,2 @@
+# space-invaders-hw
+A space invaders clone, code(ed) for the subject called programming 3 at Budapest University of Technology and Economics.
---
+++ src/main/java/en/tresz/spaceinvaders/Main.java
@@ -0,0 +1,7 @@
+package en.tresz.spaceinvaders;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
